8e7b54704b39e8e5b40a8870b80908872a5e2be7
Steven Edgar Phiri <stevenphiri151@gmail.com>
2024-10-16 14:22:05 +0200

    Test 12

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/trume/',  // Replace 'trume_website' with your actual repo name
+  base: '/trume/',  // Replace 'trume_website' with your actual repo name
 })
 
